897241beffbdc91e1e2ab30c3d837a833a5a17d9
Justin Mason <Justin.Mason@swrve.com>
2018-06-18 16:55:37 +0000

    Try a commit

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,11 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
+require 'scraperwiki'
+require 'mechanize'
+
+agent = Mechanize.new
+
 # # Read in a page
 # page = agent.get("http://foo.com")
 #
@@ -23,3 +23,4 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+#
